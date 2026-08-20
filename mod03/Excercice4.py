84057fa0c887bc763ca85173ae6b32d94f0dcbcb
Anass Mohattan <anass.mohattan@gmail.com>
2026-08-20 11:24:31 +0300

    created and rehearsed in Excercise4

---
+++ mod03/Excercice4.py
@@ -0,0 +1,9 @@
+## Not the excercice but practice
+
+lunch_time = 1145
+print("My lunch time today will be " + str(lunch_time))
+print(f"My lunch time today will be {lunch_time}")
+#print("My lunch time today will be" + lunch_time) bad command
+print(lunch_time)
+
+print(f"My lunch time today will be {lunch_time:20.1f}")
