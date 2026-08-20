d9b5d9f84b04e6ea48941fff0dde62c518f8ecc8
Anass Mohattan <anass.mohattan@gmail.com>
2026-08-20 10:27:25 +0300

    used number in a new file

---
+++ mod03/number.py
@@ -0,0 +1,19 @@
+first = -9
+second = 12_456_123_180
+third = 4.973
+third1 = 4,973
+fourth = -4 + 2j
+
+print(first)
+print(second)
+print(third)
+print(third1)
+print(fourth)
+print(fourth.real)
+print(fourth.imag)
+
+print()
+print()
+
+print(third + 1)
+print(third1)
