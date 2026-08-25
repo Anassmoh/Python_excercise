e1db7452d9a0a104eddc6bb28bfc65d2b7d1d9fd
Anass Mohattan <anass.mohattan@gmail.com>
2026-08-25 14:16:49 +0300

    new if test2

---
+++ test2.py
@@ -0,0 +1,11 @@
+money = float(input("Give money: "))
+age = float(input("give age :"))
+
+print(money >= 10)
+print(age >= 15)
+
+if money >= 10 and age >= 15:
+#if true and True
+    print("You may buy the energy drink")
+else:
+    print("Get out")
