077c11ceb77fafd64dfec0503c95c7ed64bb3e4d
Anass Mohattan <anass.mohattan@gmail.com>
2026-09-16 00:23:09 +0300

    changed  exerciae 3 to better mod05

--- mod05/Exercise3.py
+++ mod05/Exercise3.py
@@ -1,18 +1,10 @@
 number = input("Enter a number (or press Enter to quit): ")
-smallest = largest = number
-
+smallest, largest = float(number), float(number)
 while number != "":
-    number = float(number)
-    smallest = float(smallest)
-    largest = float(largest)
-    if number < smallest:
-        smallest = number
-    elif number > largest:
-        largest = number
+    if float(number) >= largest:
+        largest = float(number)
+    elif float(number) <= smallest:
+        smallest = float(number)
     number = input("Enter a number (or press Enter to quit): ")
 
-if largest == "":
-    print("Smallest number: 0.0 \nLargest number: 0.0")
-# considering the first input is an empty string
-else:
-    print(f"Smallest number: {smallest:.1f} \nLargest number: {largest:.1f}")
+print(f"Smallest number: {smallest}\nLargest number: {largest}")
