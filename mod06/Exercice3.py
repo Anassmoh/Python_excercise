699a604df1c3ac5e5ec9b9374c988fe008ad5203
Anass Mohattan <anass.mohattan@gmail.com>
2026-09-16 00:58:52 +0300

    redone exercise3 mod06 better way

--- mod06/Exercice3.py
+++ mod06/Exercice3.py
@@ -1,10 +1,10 @@
 number = int(input("Enter an integer: "))
-result = 0
-for n in range(number):
-    test = number % (number - n)
-    if test == 0:
-        result += 1
-if result != 2:
-    print(number, "is not a prime number.")
-else: 
+test = []
+for i in range(number):
+    modulo = number % (i+1)
+    test.append(modulo)
+
+if test.count(0) == 2:
     print(number, "is a prime number.")
+else:
+    print(number, "is not a prime number.")
