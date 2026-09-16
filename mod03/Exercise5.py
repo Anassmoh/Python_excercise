71b60c9ee021efc4135c802ff59155ead0ec3686
Anass Mohattan <anass.mohattan@gmail.com>
2026-09-17 00:44:49 +0300

    used floor division for EX5  smarter than old solution

--- mod03/Exercise5.py
+++ mod03/Exercise5.py
@@ -3,7 +3,7 @@ pounds = float(input("Enter pounds: "))
 lots = float(input("Enter lots: "))
 
 total_grams = 13.3 * (talents * 20 * 32 + pounds * 32 + lots)
+kilograms = total_grams // 1000
 remaining_grams = total_grams % 1000
-kilograms = (total_grams - remaining_grams) / 1000
 
 print(f"The weight in modern units: \n{kilograms:.0f} kilograms and {remaining_grams:.2f} grams.")
