e395dcb6a7ef001ab176056fe60fab51d95dea86
Anass Mohattan <anass.mohattan@gmail.com>
2026-09-23 17:58:06 +0300

    finished mod10

---
+++ project/test.py
@@ -0,0 +1,41 @@
+class MetalFound:
+    def __init__(self, name, voltage):
+        self.name = name
+        self.voltage = voltage
+        self.weight = 0
+        self.matter = 0
+
+class AllFound:
+    def __init__(self):
+        self.net = []    
+    def collect(self, item):
+        self.net.append(item)
+
+class RoadsideFound:
+    def __init__(self):
+        self.net = []    
+    def collect(self, item):
+        self.net.append(item)
+
+class BeachFound:
+    def __init__(self):
+        self.net = []
+    def collect(self, item):
+        self.net.append(item)
+
+class LakeFound:
+    def __init__(self):
+        self.net = []    
+    def collect(self, item):
+        self.net.append(item)
+
+
+item1 = MetalFound("ring", 20)
+item1.matter = "gold"
+item1.weight = item1.voltage * 15
+print("Item added to the net!\n")
+print(f"{item1.name}  {item1.voltage} {item1.weight} {item1.matter}")
+
+all = AllFound()
+all.collect(item1)
+print(all.net[0].name)
