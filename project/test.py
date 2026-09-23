263faf7d96894f873404a096b716e29c9c816d17
Anass Mohattan <anass.mohattan@gmail.com>
2026-09-23 18:34:53 +0300

    started making sense

--- project/test.py
+++ project/test.py
@@ -2,27 +2,41 @@
 #TODO: can have a list and for loop to display list contents
 #TODO: use assosiation or inheritence to append stuff in list
 #TODO: use Class variable to see how many items have been created
-class MetalFound:
+class AllNet:
+    item_count = 0
+    all_net = []
     def __init__(self, name, voltage):
+        AllNet.all_net.append(self)
         self.name = name
         self.voltage = voltage
         self.weight = 0
         self.matter = 0
 
-class AllFound:
-    def __init__(self):
-        self.net = []    
+    def collect_print(self):
+        print(f"{self.name}: {self.voltage} {self.weight} {self.matter}")
 
-    def collect(self, item):
-        self.net.append(item)
+class RoadsideNet(AllNet):
+    def __init__(self, name, voltage,):
+        super().__init__(name, voltage)
+
+class BeachNet(AllNet):
+    def __init__(self, name, voltage,):
+        super().__init__(name, voltage)
+
+class LakeNet(AllNet):
+    def __init__(self, name, voltage,):
+        super().__init__(name, voltage)
 
 
-item1 = MetalFound("ring", 20)
-item1.matter = "gold"
-item1.weight = item1.voltage * 15
-print("Item added to the net!\n")
-print(f"{item1.name}  {item1.voltage} {item1.weight} {item1.matter}")
+roadside_net, beach_net, lake_net = [], [], []
 
-all = AllFound()
-all.collect(item1)
-print(all.net[0].name)
+roadside_net.append(RoadsideNet("ring", 130))
+roadside_net.append(RoadsideNet("tin", 10))
+roadside_net.append(RoadsideNet("nail", 41))
+
+print(AllNet.item_count)
+for item in roadside_net:
+    item.collect_print()
+
+for item in AllNet.all_net:
+    item.collect_print()
